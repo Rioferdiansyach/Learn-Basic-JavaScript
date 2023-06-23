113eb51c286c1ffcfacc452ff0d31e6692db3052
Rioferdiansyah <rioferdiansyah027@gmail.com>
2023-06-23 15:56:49 +0700

    Membuat file latihanJS, looping atau perulangan

---
+++ for.js
@@ -0,0 +1,13 @@
+// Latihan Membuat looping atau perulangan dengan Pengulangan FOR
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+  console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik`);
+  noAngkot++;
+}
+for(var noAngkot;noAngkot <= jmlAngkot ;noAngkot++ ){
+  console.log(`Angkot No. ${noAngkot} Sedang tidak beroperasi dengan baik`);
+  
+}
---
+++ while.js
@@ -0,0 +1,15 @@
+// membuat looping atau perulangan untuk mencetak hello world menjadi 10 kali ulangan
+
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 10){
+//   console.log('Hello World!' +nilaiAwal+ 'x');
+//   nilaiAwal++
+// }
+
+
+// Latihan Membuat looping perulangan untuk mencetak angkot
+var angkot = 1;
+while (angkot <= 10) {
+  console.log(`Angkot No. ${angkot} Beroperasi dengan baik`);
+  angkot++
+}
